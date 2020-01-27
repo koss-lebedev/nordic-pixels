b7d02badc9c6f26c037482a409e64689c6727dd4
Constantine Lebedev <koss.lebedev@gmail.com>
2020-01-27 16:06:57 +0100

    Add SVG animation

--- src/components/complete.tsx
+++ src/components/complete.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {
   onComplete: () => void
@@ -9,27 +10,56 @@ const Complete = ({ onComplete }: Props) => {
     <div className="complete-screen">
       <svg viewBox="0 0 53.19 53.19">
         <g>
-          <circle
+          <motion.circle
             stroke="#51e051"
             strokeWidth={3}
             fill="transparent"
             cx={26.59}
             cy={26.59}
             r={24}
+            initial={{
+              scale: 0,
+              opacity: 0,
+            }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+            }}
           />
-          <path
+          <motion.path
             stroke="#51e051"
             strokeWidth={3}
             strokeLinecap="round"
             fill="transparent"
             d="M12.29 26.59l10.98 10.42 17.49-18.23"
+            initial={{
+              opacity: 0,
+              pathLength: 0,
+            }}
+            animate={{
+              opacity: 1,
+              pathLength: 1,
+            }}
+            transition={{
+              delay: 0.2,
+              duration: 0.5,
+            }}
           />
         </g>
       </svg>
 
-      <h1>
+      <motion.h1
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.7,
+          repeatDelay: 1,
+        }}
+        onAnimationComplete={() => onComplete()}
+      >
         Thank you for <br /> your order
-      </h1>
+      </motion.h1>
     </div>
   )
 }
